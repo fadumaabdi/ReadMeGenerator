13a0d5f1675887a2de34b6cecc51f57b6e6cc66a
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-06-29 19:45:08 +0100

    gmarkdowndescrpcontents

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -19,7 +19,6 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 
-
 function renderLicenseSection(license) {
   if (license !== "None") {
     return "##License This project is licensed under the ${license} license.";
@@ -31,6 +30,25 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)})
+
+  ##  Description
+  ${data.description}
+
+  ## Table of Contents
+
+  *[Installation](#installation)
+  [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  *[Contributing](#contributing)
+  *[Tests](#tests)
+  *[Questions](#questions)
+
+  ## Installation
+  To install 
+
+
+
 
 `;
 }
