faefb7231b5edd3e30342835b3be2e028c849fd4
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-06-29 20:00:40 +0100

    gmarkdownending

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
@@ -7,7 +6,6 @@ function renderLicenseBadge(license) {
   return "";
 }
 
-// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if(license !== "None"){
@@ -16,7 +14,6 @@ function renderLicenseLink(license) {
   return "";
 }
 
-// TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 
 function renderLicenseSection(license) {
@@ -27,7 +24,7 @@ function renderLicenseSection(license) {
   }
 
 
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.license)})
@@ -38,23 +35,45 @@ function generateMarkdown(data) {
   ## Table of Contents
 
   *[Installation](#installation)
+
   [Usage](#usage)
+
   ${renderLicenseLink(data.license)}
+
   *[Contributing](#contributing)
+
   *[Tests](#tests)
+
   *[Questions](#questions)
 
   ## Installation
-  To install dependencies, run the following comman:
+  To install dependencies, run the following command:
   
   \"\"\"
   ${data.installation}
   \"\"\"
 
-  #
+  ## Usage
+  ${data.usage}
+  ${renderLicenseSection(data.license)}
 
+  ## Contributing
 
+  ${data.contributing}
 
+  ##Tests
+  
+  To run tests, run the following command
+  \"\"\"
+  ${data.test}
+  \"\"\"
+
+  ## Questions
+
+  If you have questions about the repo, contact me at
+  ${data.email}. You can find my work at [${data.github}}(https://github.com/${data.github}/)).
+  ";
+  }
 
 `;
 }
